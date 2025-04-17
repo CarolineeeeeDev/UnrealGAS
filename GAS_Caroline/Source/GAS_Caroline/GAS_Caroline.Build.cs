fe614621eb85396e94278985c138d011ad89464f
Caroline <LilScorpionC@gmail.com>
2025-04-16 18:00:39 -0600

    add gameplay ability plugin

--- GAS_Caroline/Source/GAS_Caroline/GAS_Caroline.Build.cs
+++ GAS_Caroline/Source/GAS_Caroline/GAS_Caroline.Build.cs
@@ -8,7 +8,7 @@ public class GAS_Caroline : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
